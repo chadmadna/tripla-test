089f72100b13963d70a2ef53bc3b9db6e3803074
irsyad nabil <irsyad.nabil@littlelives.com>
2025-04-08 02:40:55 +0700

    fix: include required jwt revocation strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticatable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   belongs_to :publisher
   has_many :permissions, through: :roles
